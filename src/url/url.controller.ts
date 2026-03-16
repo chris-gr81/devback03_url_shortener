427a961ebe8dc7e9735b1601b248868f3d64b7d0
Christian Grimm <grimmchristian@gmx.de>
2026-03-16 10:21:25 +0100

    update error handling

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -6,76 +6,89 @@ import {
   updateByShort,
 } from "./url.service";
 import { isValidLongUrl, isValidShortId } from "../util/transform.utils";
+import { CreateUrlDto } from "./url.interface";
 
 const urlRouter = Router();
 
-urlRouter.post("/shorten", (req: Request, res: Response) => {
-  try {
-    const { longUrl } = req.body;
+urlRouter.post(
+  "/shorten",
+  (req: Request<{}, {}, CreateUrlDto>, res: Response) => {
+    try {
+      const { longUrl } = req.body;
 
-    if (!isValidLongUrl(longUrl)) {
-      return res.status(400).json({ error: "Please provide a valid URL" });
+      if (!isValidLongUrl(longUrl)) {
+        return res.status(400).json({ error: "Please provide a valid URL" });
+      }
+
+      const shortId = createShortId(longUrl);
+
+      return res.status(201).json({ shortId });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to create short ID" });
     }
+  },
+);
 
-    const shortId = createShortId(longUrl);
+urlRouter.get(
+  "/:shortId",
+  (req: Request<{ shortId: string }>, res: Response) => {
+    try {
+      const { shortId } = req.params;
 
-    return res.status(201).json({ shortId });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Failed to create short ID" });
-  }
-});
+      if (!isValidShortId(shortId)) {
+        return res.status(400).json({ error: "Please provide a valid ID" });
+      }
 
-urlRouter.get("/:shortId", (req: Request, res: Response) => {
-  try {
-    const { shortId } = req.params;
+      const longUrl = getLongUrl(shortId);
 
-    if (!isValidShortId(shortId)) {
-      return res.status(400).json({ error: "Please provide a valid ID" });
+      return res.redirect(302, longUrl);
+    } catch (err) {
+      console.error(err);
+      return res.status(404).json({ error: "Short URL not found" });
     }
+  },
+);
 
-    const longUrl = getLongUrl(shortId);
+urlRouter.delete(
+  "/:shortId",
+  (req: Request<{ shortId: string }>, res: Response) => {
+    try {
+      const { shortId } = req.params;
+      if (!isValidShortId(shortId)) {
+        return res.status(400).json({ error: "Please provide a valid ID" });
+      }
 
-    return res.redirect(302, longUrl);
-  } catch (err) {
-    console.error(err);
-    return res.status(404).json({ error: "Short URL not found" });
-  }
-});
+      deleteByShort(shortId);
 
-urlRouter.delete("/:shortId", (req: Request, res: Response) => {
-  try {
-    const { shortId } = req.params;
-    if (!isValidShortId(shortId)) {
-      return res.status(400).json({ error: "Please provide a valid ID" });
+      return res.sendStatus(204);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" });
     }
+  },
+);
 
-    deleteByShort(shortId);
+urlRouter.put(
+  "/:shortId",
+  (req: Request<{ shortId: string }, {}, CreateUrlDto>, res: Response) => {
+    try {
+      const { longUrl } = req.body;
+      const { shortId } = req.params;
 
-    return res.sendStatus(204);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-urlRouter.put("/:shortId", (req: Request, res: Response) => {
-  try {
-    const { longUrl } = req.body;
-    const { shortId } = req.params;
-
-    if (!isValidShortId(shortId)) {
-      return res.status(400).json({ error: "Please provide a valid ID" });
+      if (!isValidShortId(shortId)) {
+        return res.status(400).json({ error: "Please provide a valid ID" });
+      }
+      if (!isValidLongUrl(longUrl)) {
+        return res.status(400).json({ error: "Please provide a valid URL" });
+      }
+      updateByShort(shortId, longUrl);
+      return res.status(200).send(`${shortId} successfully updated`);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" });
     }
-    if (!isValidLongUrl(longUrl)) {
-      return res.status(400).json({ error: "Please provide a valid URL" });
-    }
-    updateByShort(shortId, longUrl);
-    return res.status(200).send(`${shortId} successfully updated`);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
+  },
+);
 
 export default urlRouter;
---
+++ src/url/url.interface.ts
@@ -0,0 +1,3 @@
+export type CreateUrlDto = {
+  longUrl: string;
+};
